773ee9f39af9ecbb71249f28db9eaf00ad4cd215
iTexZoz <lucenzo.76@gmail.com>
2018-12-28 00:00:32 +0100

    update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,9 +1,8 @@
 resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 
-name 'NativeUI'
-description 'NativeUI in LUA for FiveM'
-author 'FrazzIe (https://github.com/FrazzIe)'
-version '2.1.0'
+name 'NativeUILua-Reloaded'
+description 'NativeUILua-Reloaded is UI library for FiveM designed specifically for making interface based on GTA:Online style.'
+version '1.0.0'
 
 client_scripts {
   "share/Utils.lua",
